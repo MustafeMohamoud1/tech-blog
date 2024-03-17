218575ef74d3b13ed6a183098868db6e768df1ae
mustafemohamoud <mustyymo@outlook.com>
2024-03-17 23:20:34 +0000

    app.js modified

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path'); 
 const session = require('express-session');
 const sequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,12 +12,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Create an instance of express-handlebars with your desired configurations
+const handlebars = exphbs.create({
+  /* Add your configurations here */
+});
+
 // Set Handlebars as the template engine
-app.engine('handlebars', exphbs());
+app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 // Specify the directory where Handlebars views are located
 app.set('views', path.join(__dirname, 'views'));
+
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 const sessionStore = new sequelizeStore({
   db: sequelize
